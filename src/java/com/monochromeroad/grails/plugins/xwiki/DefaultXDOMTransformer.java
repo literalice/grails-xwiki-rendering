bf2c214374151b1bfe0002654bb7eac72fe418f1
literalice <literalice@monochromeroad.com>
2011-10-19 20:53:56 +0900

    adds xdom transformation class

---
+++ src/java/com/monochromeroad/grails/plugins/xwiki/DefaultXDOMTransformer.java
@@ -0,0 +1,39 @@
+package com.monochromeroad.grails.plugins.xwiki;
+
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.rendering.block.XDOM;
+import org.xwiki.rendering.parser.Parser;
+import org.xwiki.rendering.transformation.TransformationContext;
+import org.xwiki.rendering.transformation.TransformationException;
+import org.xwiki.rendering.transformation.TransformationManager;
+
+/**
+ * Default Transform Class for XWiki macro support.
+ *
+ * @author Masatoshi Hayashi
+ */
+public class DefaultXDOMTransformer implements XDOMTransformer {
+
+    private final XWikiComponentRepository componentRepository;
+
+    public DefaultXDOMTransformer(ComponentManager componentManager) {
+        this.componentRepository = new XWikiComponentRepository(componentManager);
+    }
+
+    /**
+     * Applies XWiki macros to the XDOM
+     *
+     * @param xdom the XDOM object
+     * @param parser XDOM parser
+     */
+    public void transform(XDOM xdom, Parser parser) {
+        try {
+            TransformationManager txManager =
+                    componentRepository.lookupComponent(TransformationManager.class);
+            txManager.performTransformations(xdom, new TransformationContext(xdom, parser.getSyntax()));
+        } catch (TransformationException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+}
---
+++ src/java/com/monochromeroad/grails/plugins/xwiki/XDOMTransformer.java
@@ -0,0 +1,21 @@
+package com.monochromeroad.grails.plugins.xwiki;
+
+import org.xwiki.rendering.block.XDOM;
+import org.xwiki.rendering.parser.Parser;
+
+/**
+ * XDOM Transform Class
+ *
+ * @author Masatoshi Hayashi
+ */
+public interface XDOMTransformer {
+
+    /**
+     * Transforms the XDOM Object
+     *
+     * @param xdom the XDOM object
+     * @param parser XDOM parser
+     */
+    public void transform(XDOM xdom, Parser parser);
+
+}
