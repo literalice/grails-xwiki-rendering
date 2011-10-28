28a8b0771a271fb341c5bd908a6cc9aea9ee0f67
literalice <literalice@monochromeroad.com>
2011-10-28 22:03:38 +0900

    increments version to 0.3

--- XwikiRenderingGrailsPlugin.groovy
+++ XwikiRenderingGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import com.monochromeroad.grails.plugins.xwiki.XWikiConfigurationProvider
 
 class XwikiRenderingGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "* > 1.3.0"
     // the other plugins this plugin depends on
