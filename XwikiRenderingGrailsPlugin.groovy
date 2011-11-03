a86af4cfb556db2b5e1f8cb5986547a89bdea7c6
literalice <literalice@monochromeroad.com>
2011-11-04 00:38:46 +0900

    adds target grails version

--- XwikiRenderingGrailsPlugin.groovy
+++ XwikiRenderingGrailsPlugin.groovy
@@ -13,7 +13,7 @@ class XwikiRenderingGrailsPlugin {
     // the plugin version
     def version = "0.3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "* > 1.3.0"
+    def grailsVersion = "* > 1.2.2"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
