4167677eaf4992b570d47dc81a104940b76f5475
literalice <literalice@monochromeroad.com>
2012-08-03 01:42:49 +0900

    increments the plugin version

--- XwikiRenderingGrailsPlugin.groovy
+++ XwikiRenderingGrailsPlugin.groovy
@@ -11,7 +11,7 @@ import org.xwiki.rendering.macro.Macro
 
 class XwikiRenderingGrailsPlugin {
     // the plugin version
-    def version = "0.6"
+    def version = "0.7"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "* > 1.2.2"
