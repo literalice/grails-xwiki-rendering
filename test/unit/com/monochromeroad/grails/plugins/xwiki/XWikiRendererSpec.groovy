2d1038618ef9b9583245bed375226e74d27e6b9d
literalice <literalice@monochromeroad.com>
2012-08-23 22:36:52 +0900

    adds macros tests

--- test/unit/com/monochromeroad/grails/plugins/xwiki/XWikiRendererSpec.groovy
+++ test/unit/com/monochromeroad/grails/plugins/xwiki/XWikiRendererSpec.groovy
@@ -1,5 +1,7 @@
 package com.monochromeroad.grails.plugins.xwiki
 
+import com.monochromeroad.grails.plugins.xwiki.macro.DateMacro
+import com.monochromeroad.grails.plugins.xwiki.macro.RbMacro
 import org.xwiki.rendering.syntax.Syntax
 import spock.lang.Specification
 import spock.lang.Shared
@@ -18,6 +20,8 @@ class XWikiRendererSpec extends Specification {
 
         XWikiConfigurationProvider configurationProvider = new XWikiConfigurationProvider();
         XWikiComponentManager componentManager = new XWikiComponentManager(getClass().classLoader);
+        componentManager.registerMacro(DateMacro)
+        componentManager.registerMacro(RbMacro)
 
         renderer = new XWikiRenderer(componentManager, configurationProvider)
     }
@@ -45,7 +49,7 @@ text :**bold**
         result == expectedText
     }
 
-    def "Macro Support"() {
+    def "Embedded Macro Support"() {
         String text = """
 = level1 =
 == level2 ==
@@ -59,7 +63,21 @@ this line is comment.
         then:"The text written in Comment Macro is not rendered"
         result == '<h1 id="Hlevel1"><span>level1</span></h1><h2 id="Hlevel2"><span>level2</span></h2>'
     }
-    
+
+    def "User Macro Support"() {
+        String text = """
+{{date/}}
+
+{{rb read="reading"}}TEXT{{/rb}}
+
+"""
+
+        when:
+        def result = renderer.render(text, Syntax.XWIKI_2_1, Syntax.XHTML_1_0)
+        then:
+        result == new Date().format("yyyy/MM/dd") + '<p><ruby>TEXT<rp>(</rp><rt>reading</rt><rp>)</rp></ruby></p>'
+    }
+
     def "Extra API: using default input syntax and default output syntax"() {
         when:
         String result = renderer.render(testXWiki21Text)
