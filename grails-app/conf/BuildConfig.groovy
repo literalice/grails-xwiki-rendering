a77cce72f9649babac339cceab46a34ed14e3e75
literalice <literalice@monochromeroad.com>
2013-05-07 07:23:12 +0900

    updates xwiki to 5.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,7 +56,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://nexus.xwiki.org/nexus/content/groups/public"
     }
     dependencies {
-        def xwikiVersion = "4.4"
+        def xwikiVersion = "5.0"
         compile("org.xwiki.commons:xwiki-commons-component-default:$xwikiVersion",
                 "org.xwiki.rendering:xwiki-rendering-transformation-macro:$xwikiVersion"){
             excludes "xercesImpl", "slf4j-api"
